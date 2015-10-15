f2da1740f176dec738d481cd4f9dea94e74aaa95
Alec Brickner <abrickne@ucsd.edu>
2015-10-14 23:37:58 -0700

    Fixed module 4 part 1 grader

--- mod4/part1/spelling/DictionaryGrader.java
+++ mod4/part1/spelling/DictionaryGrader.java
@@ -14,49 +14,84 @@ public class DictionaryGrader {
 
         int incorrect = 0;
         int tests = 0;
-        String feedback = "Some tests failed. Please check the following: ";
+        String feedback = "";
 
         try {
             Dictionary dictLL = new DictionaryLL();
 
+            feedback += "** Test #1: Adding new word to the LL dictionary...";
             if (!dictLL.addWord("tEst")) {
                 incorrect++;
-                feedback += "adding a new word to the LL dictionary returned false; ";
+                feedback += "FAILED. Adding a new word to the LL dictionary returned false. ";
+            }
+            else {
+                feedback += "PASSED. ";
             }
 
+            feedback += "** Test #2: Looking up word from last test...";
             if (!dictLL.isWord("teSt")) {
                 incorrect++;
-                feedback += "inserted word is not found in LL dictionary - make sure the word is converted to lowercase on both entry and lookup; ";
+                feedback += "FAILED. Inserted word is not found in LL dictionary. Make sure the word is converted to lowercase on both entry and lookup. ";
+            }
+            else {
+                feedback += "PASSED. ";
             }
 
+            feedback += "** Test #3: Adding a second word...";
             dictLL.addWord("second");
-
             if (dictLL.size() != 2) {
                 incorrect++;
-                feedback += "incorrect size of LL dictionary after insertion of two words; ";
+                feedback += "FAILED. Incorrect size of LL dictionary after insertion of two words. ";
+            }
+            else {
+                feedback += "PASSED. ";
             }
 
             tests += 3;
 
             Dictionary dictBST = new DictionaryBST();
 
+            feedback += "** Test #4: Adding a new word to the BST dictionary...";
             if (!dictBST.addWord("tEst")) {
                 incorrect++;
-                feedback += "adding a new word to the BST dictionary returned false; ";
+                feedback += "FAILED. Adding a new word to the BST dictionary returned false. ";
+            }
+            else {
+                feedback += "PASSED. ";
             }
 
+            feedback += "** Test #5: Retrieving the word from the last test...";
             if (!dictBST.isWord("teSt")) {
                 incorrect++;
-                feedback += "inserted word is not found in BST dictionary - make sure the word is converted to lowercase on both entry and lookup; ";
+                feedback += "FAILED. Inserted word is not found in BST dictionary. Make sure the word is converted to lowercase on both entry and lookup; ";
+            }
+            else {
+                feedback += "PASSED. ";
             }
 
+            feedback += "** Test #6: Adding second word to BST dictionary...";
             dictBST.addWord("second");
-
             if (dictBST.size() != 2) {
                 incorrect++;
-                feedback += "incorrect size of BST dictionary after insertion of two words; ";
+                feedback += "FAILED. Incorrect size of BST dictionary after insertion of two words; ";
             }
-            tests += 3;
+            else {
+                feedback += "PASSED. ";
+            }
+
+            feedback += "** Test #7: Adding lots of words and retrieving some...";
+            dictBST.addWord("seconds");
+            dictBST.addWord("seconded");
+            dictBST.addWord("secondhand");
+            dictBST.addWord("selma");
+            if (!(dictBST.isWord("seconded") && dictBST.isWord("selma"))) {
+                incorrect++;
+                feedback += "FAILED. Could not find 'seconded' and 'selma'. ";
+            }
+            else {
+                feedback += "PASSED. ";
+            }
+            tests += 4;
         } catch (Exception e) {
             out.println("{\"fractionalScore\": 0.0, \"feedback\": \"Runtime error: " + e + "\"}");
             out.close();
@@ -64,7 +99,10 @@ public class DictionaryGrader {
         }
 
         if (incorrect == 0) {
-            feedback = "You passed all the tests!";
+            feedback += "Congrats! You passed all the tests!";
+        }
+        else {
+            feedback = "Some tests failed. Please check the following: " + feedback;
         }
 
         out.println("{\"fractionalScore\": " + ((float)(tests - incorrect) / tests) + ", \"feedback\": \"" + feedback + "\"}");
